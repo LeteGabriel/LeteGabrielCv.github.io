9ee8f5f7883fc7b73e5cb65a7499cb8b15b03751
LeteGabriel <115379832+LeteGabriel@users.noreply.github.com>
2023-02-28 17:46:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+document.getElementById('nm').addEventListener('mouseenter', function(){
+            document.getElementById("nom").innerHTML = "My name Is";
+            document.getElementById("nomb").innerHTML = "Paul Foster"
+});
+
+document.getElementById('ma').addEventListener('mouseenter', function(){
+    document.getElementById("nom").innerHTML = "My mail is";
+    document.getElementById("nomb").innerHTML = "paul.foster@example.com";
+});
+
+document.getElementById('fe').addEventListener('mouseenter', function(){
+document.getElementById("nom").innerHTML = "My birthday is";
+    document.getElementById("nomb").innerHTML = "10/4/1959";
+});
+
+document.getElementById('lo').addEventListener('mouseenter', function(){
+    document.getElementById("nom").innerHTML = "My address is";
+    document.getElementById("nomb").innerHTML = "1037 Crockett St";
+});
+
+document.getElementById('ph').addEventListener('mouseenter', function(){
+    document.getElementById("nom").innerHTML = "My phone number is";
+    document.getElementById("nomb").innerHTML = "(378) 925-8740";
+});
+
